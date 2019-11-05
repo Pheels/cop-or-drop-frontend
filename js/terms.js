89a3d1bfd32797f37bf45f1d792c033321deee34
Oliver McPheely <o.mcpheely@gmail.com>
2019-11-05 15:32:47 +0000

    Added google drive to terms

--- js/terms.js
+++ js/terms.js
@@ -4,21 +4,21 @@ function displayDocument(name){
       var html = document.getElementById("privacycookies").innerHTML = `
         <div onclick="closeDocument('privacy')" class="h33 up">Privacy and cookies policy
         <img src="images/down.png" height="35px" width="35px" style="float:right"></div>
-        <embed src="files/Privacy_and_Cookie_Policy.pdf" width="60%" height="750px" style="padding-bottom:50px" type="application/pdf">
+        <iframe src="https://drive.google.com/file/d/15M74lXZiV4708au-jjhkDYlGQ8Gi3wPX/preview" width="60%" height="580"</iframe>
         `
       break;
     case 'refund':
     var html = document.getElementById("refund").innerHTML = `
       <div onclick="closeDocument('refund')" class="h33 up">Refund Policy
       <img src="images/down.png" height="35px" width="35px" style="float:right"></div>
-      <embed src="files/Refund_Policy.pdf" width="60%" height="750px" style="padding-bottom:50px" type="application/pdf">
+      <iframe src="https://drive.google.com/file/d/1ViGi71kio7QN3H_95rxYPza-pZcUxKxa/preview" width="60%" height="580"</iframe>
       `
       break;
     case 'terms':
     var html = document.getElementById("termsconditions").innerHTML = `
-      <div onclick="closeDocument('terms')" class="h33 up">Terms and Conditions
+      <div onclick="closeDocument('terms')" class="h33 up">Terms and Conditions&nbsp&nbsp&nbsp&nbsp
       <img src="images/down.png" height="35px" width="35px" style="float:right"></div>
-      <embed src="files/Terms_and_Conditions.pdf" width="60%" height="750px" style="padding-bottom:50px" type="application/pdf">
+      <iframe src="https://drive.google.com/file/d/15M74lXZiV4708au-jjhkDYlGQ8Gi3wPX/preview" width="60%" height="580"</iframe>
       `
       break
   }
@@ -38,7 +38,7 @@ function closeDocument(name){
       break;
     case 'terms':
     var html = document.getElementById("termsconditions").innerHTML = `
-    <div onclick="displayDocument('terms')"  class="h33">Terms and Conditions
+    <div onclick="displayDocument('terms')"  class="h33">Terms and Conditions&nbsp&nbsp&nbsp&nbsp
     <img src="images/up.png" height="35px" width="35px" style="float:right"></div>`
 
 
